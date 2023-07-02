5e71ad114a0960871c706cc57d04eb8d407f1861
Alex Almeida <alex@oddadigitalsystem.no>
2023-07-02 21:57:46 +0200

    feat(wip): remove old minimal api and replace to new model

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,38 @@
-namespace API
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
+using System.Security.Cryptography;
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+using CheapestTravel;
+
+using Core;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace API;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    var app = builder.Build();
+
+    app.MapGet("/api/route/{from}/{destination}", (string from, string destination) =>
+    {
+      var network = new FlightNetwork(new FileRepository(filePath: "../Core/store/input-routes.csv"));
+      var route = network.CheapRoute(from, destination);
+      return Results.Ok(route);
+    });
+
+    app.MapPost("/api/route", (Route route) =>
+    {
+      var repo = new FileRepository(filePath: "../Core/store/input-routes.csv");
+      repo.AddRoute(route.From, route.Destination, route.Cost);
+      repo.Save();
+      return Results.Ok(route);
+    });
+
+    app.Run();
   }
 }
