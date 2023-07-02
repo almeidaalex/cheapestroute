a0157841a4c21a749db03b774593a724ddadfc23
Alex Almeida <alex@oddadigitalsystem.no>
2023-07-02 15:44:27 +0200

    feat(wip): create more decent cli interface

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,53 +1,31 @@
 ﻿using CheapestTravel;
 
 using System;
-using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using Microsoft.VisualBasic;
 
-namespace CLI
+namespace CLI;
+
+internal class Program
 {
-  class Program
+  private static async Task<int> Main(string[] args)
   {
-    static void Main(string[] args)
+    var root = new RootCommand
     {
-      //What I need to improve here?
-      //Move all these methods to a class that nows how to import from a path
-      //FlightNetwork.ImportFromFile("path")
-      if (args.Length == 0)
-        Console.WriteLine("File with routes must be provided");
+      new Option<string>("--import-file", description: "Import CSV file to Cheap Flight System"),
+      new Option<string>("--trip", description: "Import CSV file to Cheap Flight System"),
+    };
 
-      var routes = RouteFile.ReadFile(args[0]);
-      var network = new FlightNetwork();
-      var loadResult = network.LoadFrom(routes);
+    var tripCommand = new Command("--trip", description: "Find the cheapest route between two points");
+    tripCommand.SetHandler((trip) => Console.WriteLine($"Trip: {trip}"));
 
-      if (loadResult.HasErrors)
-        Console.WriteLine(string.Join(Environment.NewLine, loadResult.Errors));
-      else
-        while (true)
-        {
-          Console.Write("please enter the route: ");
-          var route = Console.ReadLine();
+    root.Handler = CommandHandler.Create<string, string>((importFile, trip) => {
+      Console.WriteLine($"Import: {importFile}");
+      Console.WriteLine($"Trip: {trip}");
+    });
 
-          if (!ValidateRoute(route))
-          {
-            Console.WriteLine("Invalid route input format, you should try AAA-AAA");
-            continue;
-          }
-
-          var splitedRoutes = route.Split("-");
-          var routeResult = network.CheapRoute(splitedRoutes[0], splitedRoutes[1]);
-          if (routeResult.HasErrors)
-            Console.WriteLine(string.Join(Environment.NewLine, routeResult.Errors));
-          else
-            Console.WriteLine(routeResult.ToString());
-        }
-
-    }
-
-    private static bool ValidateRoute(string givenRoute)
-    {
-      var regex = new Regex("[a-zA-Z]{1,}-[a-zA-Z]{1,}");
-      return regex.IsMatch(givenRoute);
-    }
-
+    return await root.InvokeAsync(args);
   }
 }
